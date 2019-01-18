e619624773fcd0107627fef0dc703260248326a4
rahulsharma15j <46393429+rahulsharma15j@users.noreply.github.com>
2019-01-18 20:23:39 +0530

    Update awsConfig.js
    changes

--- config/awsConfig.js
+++ config/awsConfig.js
@@ -1,7 +1,7 @@
 let awsConfig = {};
 
-awsConfig.AWS_SECRET_ACCESS_KEY = 'bD9OSaPC2NL8ptt38Z5FNpdRugGvpwkFutqUzKyx';
-awsConfig.AWS_SECRET_ACCESS_ID = 'AKIAJ7BJH5OQXMVQPI7Q';
+awsConfig.AWS_SECRET_ACCESS_KEY = '';
+awsConfig.AWS_SECRET_ACCESS_ID = '';
 awsConfig.REGION = 'us-east-1';
 
 module.exports = awsConfig;
