f91032410bc6532a6f8384ab36caaef6913264bd
Rahul Sharma <rahulsharma15j@gmail.com>
2019-07-02 15:18:53 +0530

    sdfd

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,14 +4,14 @@ appConfig.port = 3000;
 appConfig.allowedCorsOrigin = "*";
 appConfig.env = "dev";
 appConfig.db = {
-    uri:" "
-}
+  uri: `mongodb://node-shop:node-shop@cluster0-shard-00-00-zvxva.mongodb.net:27017,cluster0-shard-00-01-zvxva.mongodb.net:27017,cluster0-shard-00-02-zvxva.mongodb.net:27017/istr?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`
+};
 appConfig.apiVersion = "/api/v1";
 
 module.exports = {
-    port:appConfig.port,
-    allowedCorsOrigin:appConfig.allowedCorsOrigin,
-    environment:appConfig.env,
-    db:appConfig.db,
-    apiVersion:appConfig.apiVersion
-}
+  port: appConfig.port,
+  allowedCorsOrigin: appConfig.allowedCorsOrigin,
+  environment: appConfig.env,
+  db: appConfig.db,
+  apiVersion: appConfig.apiVersion
+};
